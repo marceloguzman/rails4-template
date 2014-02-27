3ee7e8d0d78d9b0257da882260246f5738d7b91c
Marcelo Guzmán <marcelo@marceloguzman.com>
2014-02-27 16:37:23 -0600

    first steps
    creation of folders and initial files for this template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4template::Application.config.secret_key_base = 'c6394e89680c29fbfa4b103a17efffd2265f3480148b93b6d4273814073574f92d0866af5ce1dabe79436f302affb734a28ddeb1b9149ba734fc078813fe16d5'
